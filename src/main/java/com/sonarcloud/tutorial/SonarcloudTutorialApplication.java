0e4dad6776d035c567ab0fd51f923f72abd0efeb
Marcos Manuel Arreguín Pérez <marcosmap@192.168.1.11>
2023-07-03 17:44:34 -0600

    first commit

---
+++ src/main/java/com/sonarcloud/tutorial/SonarcloudTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.sonarcloud.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarcloudTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarcloudTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sonarcloud/tutorial/SonarcloudTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonarcloud.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarcloudTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
